e0fc90d33db386682d71f57a2ee1154570e74ce4
Kyle <whiteky9@msu.edu>
2021-06-02 12:43:24 -0400

    initial

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        PrintLines pl = new PrintLines();
+        pl.printOne();
+        pl.printTwo();
+        pl.printThree();
+        pl.printFour();
+    }
+}
