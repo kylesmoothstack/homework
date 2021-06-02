37797efab46b70b39edab65f4f00b04e5fdffbeb
kylesmoothstack <85246809+kylesmoothstack@users.noreply.github.com>
2021-06-02 14:25:31 -0400

    Delete src directory

--- src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        PrintLines pl = new PrintLines();
-        pl.printOne();
-        pl.printTwo();
-        pl.printThree();
-        pl.printFour();
-    }
-}
